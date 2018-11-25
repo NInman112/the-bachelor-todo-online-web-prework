4be2fc2fb29bb7515ea45f754fd3f84213465064
Nick Inman <nick@theoatmeal.com>
2018-11-25 00:14:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_occupation(data, hometown)
     data.each do |stats|
       if stats["hometown"] == hometown
          occu << stats["occupation"]
-         return occu
+         occu
       end
     end 
   end
