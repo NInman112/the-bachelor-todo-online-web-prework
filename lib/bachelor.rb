30cb01d64bec4bc7ea9d49ef83f8f5549b68ce17
Nick Inman <nick@theoatmeal.com>
2018-11-25 00:26:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -64,7 +64,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, data|
     if seasons == season
       data.each do |stats|
-         age += stats["age"].to_i
+         age += stats["age"].to_f
          counter += 1
       end 
     end
