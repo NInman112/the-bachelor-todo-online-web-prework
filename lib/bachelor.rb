5e64b21bee4255b6d074c81d15fb954871743c02
Nick Inman <nick@theoatmeal.com>
2018-11-25 00:08:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,7 @@ def get_contestant_name(data, occupation)
     data.each do |stats|
       if stats["occupation"] == "Cruise Ship Singer"
          name << stats["name"]
-         binding.pry
+         #binding.pry
       end
     end 
   end
